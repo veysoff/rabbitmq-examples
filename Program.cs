b8e745d46432653eba55475a29f7cc412f969be3
Vitali Veys <veysoff@gmail.com>
2023-12-28 14:29:49 +0100

    added sample of sending and receiving messages

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ_Example;
+
+var client = new RabbitMqClient("localhost");
+
+for (int i = 0; i < 5; i++)
+{
+    client.Send();
+}
+
+client.Receive();
---
+++ RabbitMqClient.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ_Example;
+
+public class RabbitMqClient
+{
+    private readonly string _hostName;
+    public RabbitMqClient(string hostName)
+    {
+        _hostName = hostName;
+    }
+
+    public void Send()
+    {
+        var factory = new ConnectionFactory { HostName = _hostName };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        const string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: string.Empty,
+            routingKey: "hello",
+            basicProperties: null,
+            body: body);
+        Console.WriteLine($" [x] Sent {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    public void Receive()
+    {
+        var factory = new ConnectionFactory { HostName = _hostName };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+        };
+        channel.BasicConsume(queue: "hello",
+            autoAck: true,
+            consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
